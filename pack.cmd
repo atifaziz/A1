b230f0c09fe2c3163e4ade57d85ceb40e9493f67
Atif Aziz <code@raboof.com>
2016-12-15 09:25:36 +0100

    Send error messages in shell scripts to stderr

--- pack.cmd
+++ pack.cmd
@@ -10,10 +10,10 @@ set DOTNETEXE=
 for %%f in (dotnet.exe) do set DOTNETEXE=%%~dpnx$PATH:f
 if not defined DOTNETEXE set DOTNETEXE=%ProgramFiles%\dotnet
 if not exist "%DOTNETEXE%" (
-    echo .NET Core does not appear to be installed on this machine, which is
-    echo required to build the solution. You can install it from the URL below
-    echo and then try building again:
-    echo https://dot.net
+    echo>&2 .NET Core does not appear to be installed on this machine, which is
+    echo>&2 required to build the solution. You can install it from the URL below
+    echo>&2 and then try building again:
+    echo>&2 https://dot.net
     exit /b 1
 )
 set VERSION_SUFFIX=
