e09a6c8be7915b4db4bf79d68839c586ddd5023b
Atif Aziz <code@raboof.com>
2016-12-19 15:58:36 +0100

    Build script typo

--- build.cmd
+++ build.cmd
@@ -9,7 +9,7 @@ setlocal
 set DOTNETEXE=
 for %%f in (dotnet.exe) do set DOTNETEXE=%%~dpnx$PATH:f
 if not defined DOTNETEXE set DOTNETEXE=%ProgramFiles%\dotnet
-if not exist "%DOTNETEXE%x" (
+if not exist "%DOTNETEXE%" (
     echo>&2 .NET Core does not appear to be installed on this machine, which is
     echo>&2 required to build the solution. You can install it from the URL below
     echo>&2 and then try building again:
