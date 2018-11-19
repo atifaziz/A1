bb4563cda29f2a2aa6a13520e8a77b91a986ff9d
Atif Aziz <code@raboof.com>
2018-11-19 11:40:30 +0100

    Add code coverage reporting

--- test.cmd
+++ test.cmd
@@ -1,4 +1,11 @@
 @echo off
+setlocal
 pushd "%~dp0"
 call build && dotnet test --no-build tests
+set TEST=dotnet test --no-build tests -c
+    call build ^
+ && %TEST% Debug -p:CollectCoverage=true ^
+                 -p:CoverletOutputFormat=opencover ^
+                 -p:Exclude=[XUnit*]* ^
+ && %TEST% Release
 popd
