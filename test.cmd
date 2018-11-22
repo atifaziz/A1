b950c1586fbb9eb9e37618c4af4f3f93fb4f7026
Atif Aziz <code@raboof.com>
2018-11-22 14:13:17 +0100

    Duplicate build & test in Windows test script

--- test.cmd
+++ test.cmd
@@ -1,7 +1,6 @@
 @echo off
 setlocal
 pushd "%~dp0"
-call build && dotnet test --no-build tests
 set TEST=dotnet test --no-build tests -c
     call build ^
  && %TEST% Debug -p:CollectCoverage=true ^
